3e80868f85a7ed89ad121a4d088cd47b4d87b863
Samuel Yuen <samuel.s.yuen@gmail.com>
2021-02-06 02:20:57 -0500

    + updated portfolio component

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export FLASK_DEBUG=0
 export FLASK_APP=server:create_server
 
-flask run --host=localhost --port=12000 &
+flask run --host=localhost --port=5000 &
 
-python -mwebbrowser http://localhost:12000/ &
+python -mwebbrowser http://localhost:5000/ &
 wait
