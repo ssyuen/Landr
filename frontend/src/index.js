565a92d0ca0c390444d827780363e6f4278e0b0a
Lok <hk65962@uga.edu>
2021-02-05 20:21:39 -0500

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import {CreateRoutes} from './CreateRoutes';
+import {CreateRoutes} from './routes/CreateRoutes';
 
 
 ReactDOM.render(
