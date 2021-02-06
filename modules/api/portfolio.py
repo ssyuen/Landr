8bd2a10eb435f8bcc2b4114c496526f6666caf98
Manu P <28782438+manu-p-1@users.noreply.github.com>
2021-02-06 02:08:09 -0500

    Fixed portfolio information

--- modules/api/portfolio.py
+++ modules/api/portfolio.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import finviz
 import finnhub
+import json
 
 # This API key is already public so it doesn't make a difference
 finnhub_client = finnhub.Client(api_key="c02ql9n48v6v3d2p6ojg")
@@ -8,14 +9,23 @@ finnhub_client = finnhub.Client(api_key="c02ql9n48v6v3d2p6ojg")
 portfolio_api_bp = Blueprint('portfolio_api_bp', __name__, url_prefix='/api/portf')
 
 
-@portfolio_api_bp.route('/get_ticker', methods=['GET'])
+@portfolio_api_bp.route('/get_all_tickers', methods=['GET'])
+def get_all_tickers():
+    with open('feb2eod.json') as f:
+        data = json.load(f)
+    return jsonify(data['info'])
+
+
+@portfolio_api_bp.route('/get_tickers', methods=['GET'])
 def get_ticker():
-    all_values = finnhub_client.stock_symbols('US')
+    param = request.args.get('symbols')
+    symbol_list = param.split(",")
 
     ll = []
-    for val in all_values:
-        ll.append(finviz.get_stock(val['symbol']))
+    for symbol in symbol_list:
+        ticker_info = finnhub_client.company_basic_financials(symbol, metric='all')
+        ll.append({
+            symbol: ticker_info
+        })
 
-    return jsonify(
-        ll
-    )
+    return jsonify(ll)
