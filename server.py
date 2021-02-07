d2789df1e1b5eaaaeb8f9849391294630c8ab5c4
will-vega <vegaslaswilliam@gmail.com>
2021-02-07 06:55:10 +0000

    work plz

--- server.py
+++ server.py
@@ -4,11 +4,16 @@ from selenium import webdriver
 
 import secrets
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--headless')
-driver = webdriver.Chrome("bin/chromedriver/chromedriver.exe", chrome_options=options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--incognito')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--remote-debugging-port=9222')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('no-sandbox')
+
+driver = webdriver.Chrome("bin/chromedriver/chromedriver", options=chrome_options)
 
 def create_server():
     app = Flask(__name__)
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export FLASK_DEBUG=0
 export FLASK_APP=server:create_server
 
-flask run --host=localhost --port=5000 &
+flask run --host=10.150.0.2 --port=5000 &
 
 python -mwebbrowser http://localhost:5000/ &
 wait
